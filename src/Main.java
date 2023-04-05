5a63a3df62b538e6c7f716511aeb72d3fa052163
Andrii Lytvyn <litvin.a.a@gmail.com>
2023-04-05 13:58:43 +0200

    Add solution

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,36 @@
-public class Main {
-  public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-    System.out.println("Hello world!");
+public class Main {
+  /*Задача 1
+Найти минимальное чётное число в последовательности положительных чисел или вывести -1, если такого
+ числа не существует.
+Оценить временную и пространственную сложность алгоритма.*/
+  public static void main(String[] args) throws IOException { // O(n) - time, O(1) - space
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); // O(1)
+
+    System.out.print("Enter number of numbers: "); // O(1)
+    int n = Integer.parseInt(br.readLine()); // O(1)
+    int min = Integer.parseInt(br.readLine()); // O(1)
+
+    for (int i = 1; i < n; i++) { // O(n) O(1) - space
+      int nextNumber = Integer.parseInt(br.readLine());  //O(1) - time, O(1) - space
+
+      if (nextNumber % 2 == 0) {
+        if (min % 2 != 0) {
+          min = nextNumber;
+        }
+        if (min > nextNumber) {
+          min = nextNumber;
+        }
+      }
+    }
+    if (min % 2 == 0) {
+      System.out.println("Min even number: " + min); // O(1)
+    } else {
+      System.out.println("-1"); // O(1)
+    }
+
   }
 }
